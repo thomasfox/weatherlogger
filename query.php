76a28ec975005498d6a26d56e3d9dfa2a3387093
Thomas Fox <fox@seitenbau.de>
2018-11-30 05:24:29 +0100

    Fix titles

--- query.php
+++ query.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Wetterdaten abfragen</title>
+<title>SVS Wetter - Tagesdaten</title>
 <meta charset="UTF-8"> 
 <link rel="stylesheet" href="css/bootstrap.min.css" />
 <link rel="stylesheet" href="css/weatherlogger.css" />
